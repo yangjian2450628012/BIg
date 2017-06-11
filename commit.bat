01364ad827e7d78cec65774328aea7cb01b1494a
yangjian <yangjian_tnt@qq.com>
2017-06-11 23:54:08 +0800

    add commit.bat

---
+++ commit.bat
@@ -0,0 +1,10 @@
+git add ./pom.xml
+git add FrontMusik-Admin/pom.xml
+git add FrontMusik-Admin/src/*
+git add FrontMusik-App/pom.xml
+git add FrontMusik-App/src/*
+git add FrontMusikEngine/pom.xml
+git add FrontMusikEngine/src/*
+
+git commit -m 'update'
+git push -u origin master
