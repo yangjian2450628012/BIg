4da8c2405a6d78c091d4802b7f93e3ba5b138d15
xiaoJ <yangjian_tnt@qq.com>
2017-06-12 14:35:01 +0800

    'update'

--- FrontMusikEngine/src/main/resources/engine/http/resources/js/common.js
+++ FrontMusikEngine/src/main/resources/engine/http/resources/js/common.js
@@ -1,10 +1,12 @@
 /* engine-v1.0.0 MIT License By http://www.yobbo.tech */
 var engine = layui.use('layer',function(){
 	engine.jquery('.openTemplate-a').on('click',function(){
-		var othis = engine.jquery(this),options = othis.data(); 
+		var othis = engine.jquery(this),options = othis.data();
+		if(typeof options.options == "string"){
+			var _v = "{"+options.options+"}",options = eval('('+ _v +')');
+		}
 		engine.commonMethod[options.method] ? engine.commonMethod[options.method].call(this,options) : null;
 	});
-	
 });
 
 ;!function(){"use strict"
@@ -12,7 +14,7 @@ var engine = layui.use('layer',function(){
 			openTemplate : function(options){
 				engine.layer.open({
 					type: 2,
-					title: options['windowtitle'] ? options['windowtitle'] : '模板窗口',
+					title: options['windowTitle'] ? options['windowTitle'] : '模板窗口',
 					area: ['1200px', '500px'],
 					shade: 0,
 			        maxmin: true,
@@ -21,7 +23,7 @@ var engine = layui.use('layer',function(){
 			        btn: ['确认生成', '全部关闭'],
 			        yes: function(){
 			        	// TODO 获取IFREAM中的内容，回调
-			        	var callback = options['callbackyes'];
+			        	var callback = options['callback'];
 			        	callback ? engine[callback].call(this,options) : null;
 			            alert("确认生成");
 			        },
